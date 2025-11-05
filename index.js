2cd4fc0644246916eacd42d5ced0e747cc221f44
Justin31241 <jc20061@gmail.com>
2025-11-05 13:49:56 -0800

    Circle Drawing Added

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const canvas = document.getElementById("canvas-background"); 
-const ctx = canvas.getContext('2d');
+const myScene = canvas.getContext('2d');
+
+//circle parameters
+const radius = 50;
+
+//canvas center
+
 
 function drawGrid(squareSize) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); 
-  ctx.strokeStyle = '#ccc'; 
-  ctx.lineWidth = 0.009;
+  myScene.clearRect(0, 0, canvas.width, canvas.height); 
+  myScene.strokeStyle = '#ccc'; 
+  myScene.lineWidth = 0.009;
 
 
   for (let i = 0; i < canvas.height / squareSize; i++) {
-    ctx.beginPath();
-    ctx.moveTo(0, i * squareSize);
-    ctx.lineTo(canvas.width, i * squareSize);
-    ctx.stroke();
+    myScene.beginPath();
+    myScene.moveTo(0, i * squareSize);
+    myScene.lineTo(canvas.width, i * squareSize);
+    myScene.stroke();
   }
 
 
   for (let i = 0; i < canvas.width / squareSize; i++) {
-    ctx.beginPath();
-    ctx.moveTo(i * squareSize, 0);
-    ctx.lineTo(i * squareSize, canvas.height);
-    ctx.stroke();
+    myScene.beginPath();
+    myScene.moveTo(i * squareSize, 0);
+    myScene.lineTo(i * squareSize, canvas.height);
+    myScene.stroke();
   }
 }
 
@@ -34,3 +40,14 @@ function resizeAndDraw() {
 window.addEventListener('resize', resizeAndDraw);
 
 resizeAndDraw();
+
+function drawCircle() {
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    myScene.beginPath();
+    myScene.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+}
+
+drawCircle();
+myScene.fill();
